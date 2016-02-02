05ae5581e76d34997a7bcedc448e36e736abe899
Marina Igumnova <igum.marina@gmail.com>
2016-02-02 18:40:42 +0300

    Add demo table to /showusers

--- app.rb
+++ app.rb
@@ -4,7 +4,9 @@ require 'sinatra/reloader'
 require 'sqlite3'
 
 def get_db
-	return SQLite3::Database.new 'barbershop.db'
+	db = SQLite3::Database.new 'barbershop.db'
+	db.results_as_hash=true
+	return db
 end
 
 configure do
@@ -100,7 +102,6 @@ post '/contacts' do
 										
 end
 
-get '/showusers' do
-  erb "Hello World"
+get '/showusers'do
+	erb :showusers                                     
 end
-
